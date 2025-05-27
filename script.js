0a9cb73847ad72ab8d0f1d6f750647d3e4bc30af
atul0412 <atulpandey0412@gmail.com>
2025-05-27 10:40:53 +0530

    Refactor header styles for improved layout; adjust navigation positioning and menu button styles for better usability

--- script.js
+++ script.js
@@ -86,20 +86,20 @@ $(document).ready(function() {
   
   function updateActiveSection() {
     var scrollPosition = $(window).scrollTop();
-
+  
     // Checking if scroll position is at the top of the page
     if (scrollPosition === 0) {
       $(".header ul li a").removeClass("active");
       $(".header ul li a[href='#home']").addClass("active");
       return;
     }
-
+  
     // Iterate through each section and update the active class in the header
     $("section").each(function() {
       var target = $(this).attr("id");
       var offset = $(this).offset().top;
       var height = $(this).outerHeight();
-
+  
       if (
         scrollPosition >= offset - 40 &&
         scrollPosition < offset + height - 40
@@ -109,11 +109,3 @@ $(document).ready(function() {
       }
     });
   }
-
-  // Add event listener for menu button
-  $(".menu-btn").on("click", function() {
-    $(".header ul").toggleClass("show-menu");
-    $(this).toggleClass("open");
-  });
- 
- 
